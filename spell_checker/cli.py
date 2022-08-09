7ee7db100ea4ced4b2d3e4694b0fe45dad0efe31
Song Sun <me@sunsong.org>
2022-08-09 16:00:45 +0800

    Add chinese characters checks

--- spell_checker/cli.py
+++ spell_checker/cli.py
@@ -1,14 +1,12 @@
 """Console script for spell_checker."""
 import sys
 import click
+from spell_checker import spell_checker
 
 
 @click.command()
 def main(args=None):
-    """Console script for spell_checker."""
-    click.echo("Replace this message by putting your code into "
-               "spell_checker.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
+    spell_checker.check_chinese()
     return 0
 
 
